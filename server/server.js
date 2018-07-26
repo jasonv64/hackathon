9dcb58e2196c30bec9ee66a1cce1bd0a2e453b4b
Jason Valadez <valadezj045@gmail.com>
2018-07-26 10:08:08 -0700

    cors contol

--- server/server.js
+++ server/server.js
@@ -6,5 +6,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 module.exports = app;
